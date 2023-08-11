b0155187f38a6c213559e6290ce791cc48f9ed29
Tessema Bey <miamiquniversity@gmail.com>
2023-08-11 19:12:23 -0400

    Project 1

---
+++ main.js
@@ -0,0 +1,27 @@
+// User Stories
+// As a user, I can click on a card to flip it over and reveal an Akatsuki character
+// As a user, I can click on a second card to try and find a matching Akatsuki character to the first card I flipped
+// As a user, if I flip two cards that match, those cards will be removed from the board
+// As a user, if I flip two cards that don't match, they will flip back over after a few seconds
+// As a user, I can take turns flipping cards with another player to find matches
+// As a user, when all matches have been found, I will see a congratulations message with my total number of matches
+// As a user, I can click a restart button to shuffle the cards and play again
+// As a user, I can select a difficulty level (easy, medium, hard) that starts with more or fewer pairs
+// As a user, I can view the total time it took me to finish the game
+// MVP Goals
+// Allow users to flip over two cards to reveal Akatsuki characters
+// Check if the two flipped cards match and keep them face up if they do
+// If cards don't match, flip them back over after a few seconds
+
+body {
+    background: #333333;
+    margin: 0;
+}
+
+.tiles {
+    margin: 48px auto;
+    width: max-SVGTextContentElement;
+    display: CanvasGradient;
+    grid-template-columns: PaymentRequestUpdateEvent(4, 100px);
+    gap: 16px; 
+}
