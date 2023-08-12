d6115166b284b1ed421927deaf01f50035e1f5a7
Tessema Bey <miamiquniversity@gmail.com>
2023-08-12 09:59:45 -0400

    Updated

--- scripts.js
+++ scripts.js
@@ -1,88 +1,109 @@
-const cards = document.querySelectorAll('.memory-card');
+// Select all cards
+const cards = document.querySelectorAll('.memory-card'); 
 
+// Player scores
 let score1 = 0;
 let score2 = 0;
+
+// First clicked card 
 let isFirstCard = false;
 let first, second;
-let isBoardClosed = false;
+
+// Prevent further clicks
+let isBoardClosed = false; 
+
+// Track current player turn
 let player1Turn = true;
+
+// Card flip state
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-
-// Card flip logic
+// Handle card click 
+// Add flip class, track selected card
 function flipCard() {
-if (lockBoard) return;
-if (this === firstCard) return;
 
+  // Prevent clicks if board locked
+  if (lockBoard) return;
+  
+  // Prevent selecting same first card twice
+  if (this === firstCard) return;
 
-this.classList.add('flip');
-if (!hasFlippedCard) {
-hasFlippedCard = true;
-firstCard = this;
+  // Flip card visibility  
+  this.classList.add('flip');
+  
+  // First clicked card
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
 
+    // Exit function
+    return; 
+  }
 
-return;
+  // Assign second clicked card
+  secondCard = this;
+  
+  // Check if cards match  
+  checkForMatch();
 }
 
-
-secondCard = this;
-// Check if the two clicked cards match
-checkForMatch();
-}
-
-
-// Checking for match
+// Compare cards data attribute 
+// Trigger match or no match functions
 function checkForMatch() {
-let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-
-isMatch ? disableCards() : unflipCards();
+  // If match found
+  isMatch ? disableCards() : unflipCards(); 
 }
 
-
+// Disable matched cards
+// Increment score
+// Reset board
 function disableCards() {
-firstCard.removeEventListener('click', flipCard);
-secondCard.removeEventListener('click', flipCard);
 
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
-// Reset the board
-resetBoard();
+  resetBoard();
 }
 
-
-
+// Unflip mismatched cards 
+// Lock board temporarily
 function unflipCards() {
-lockBoard = true;
 
+  // Lock board
+  lockBoard = true; 
+  
+  // Timer to unflip  
+  setTimeout(() => {
+    
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
-setTimeout(() => {
-firstCard.classList.remove('flip');
-secondCard.classList.remove('flip');
+    // Reset variables  
+    resetBoard();
 
-
-// Reset the board
-resetBoard();
-}, 1500);
+  }, 1500);
 }
 
-
-// Resetting the board
+// Reset state 
 function resetBoard() {
-[hasFlippedCard, lockBoard] = [false, false];
-[firstCard, secondCard] = [null, null];
+  hasFlippedCard = false;
+  lockBoard = false;
+
+  firstCard = null;
+  secondCard = null;
 }
 
-
-// Shuffling card order
+// Shuffle card order randomly
 (function shuffle() {
-cards.forEach(card => {
-let randomPos = Math.floor(Math.random() * 12);
-card.style.order = randomPos;
-});
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
 })();
 
-
-// Attach click handler
+// Attach flip handler to each card
 cards.forEach(card => card.addEventListener('click', flipCard));
