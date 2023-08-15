7f36f48d7660b97cd83deccb8c77e26ca21f2784
Tessema Bey <miamiquniversity@gmail.com>
2023-08-15 18:10:30 -0400

    Updated

--- scripts.js
+++ scripts.js
@@ -1,110 +1,66 @@
 // Select all cards
 const cards = document.querySelectorAll('.memory-card');
-console.log("Selecting all cards");
 
-//Variables for players
+// Player objects
 const player1 = {
-  score: 0,
-  name: 'Player 1'
-}
+  name: 'Player 1',
+  score: 0 
+};
 
 const player2 = {
-  score: 0, 
-  name: 'Player 2'
-}
-console.log("Created player objects");
+  name: 'Player 2',
+  score: 0
+};
 
-// Default starting player 
+// Starting player
 let currentPlayer = player1;
-console.log("Starting player is Player 1");
 
-let playerTurn = 1; // start with player 1
-
-// After each turn, switch players
-function switchPlayer() {
-  playerTurn = playerTurn === 1 ? 2 : 1; 
-}
-
-
-// DOM elements for score and prompt
+// DOM elements
 const player1ScoreElement = document.getElementById('player1-score');
 const player2ScoreElement = document.getElementById('player2-score');
-const currentPlayerPrompt = document.getElementById('player-prompt');
-console.log("Got DOM elements");
 
-// Track first clicked card
+// Track card state
 let firstCard;
-
-// Prevent further clicks if board locked
-let lockBoard = false; 
-
-// Track if card flipped  
+let secondCard;
 let hasFlippedCard = false;
+let lockBoard = false; 
 
 // Handle card click
 function flipCard() {
-  // Pseudocode
-  // - Check current player
-  // - Update UI
-
-  // Get current player
-  const currentPlayer = getCurrentPlayer();
-
-  // Log player
-  console.log("Current player: ", currentPlayer);
-
-  // Update UI
-  updatePlayerUI(currentPlayer);
-  
-  // Flip card
-  this.flipped = true;
-  console.log("Flipped card");
-  
-  // Check for lock
-  if (lockBoard) return;
-
-  // Check first card
-  if (this === firstCard) return;  
 
   // Add flip class
   this.classList.add('flip');
-  console.log("Added flip class");
+  console.log('Card flipped!');
 
-  // Set first card
-  if (!hasFlippedCard) {
+  // First card logic  
+  if(!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
     return;
   }
 
+  // Set second card
+  secondCard = this;
+  
   // Check match
-  checkForMatch(this);
+  checkForMatch(secondCard);
 
 }
 
-// Check match
+// Check for match
 function checkForMatch(secondCard) {
-
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  console.log('Checking for match...');
 
-  if (isMatch) {
-    disableCards();
-  } else {  
-    unflipCards();
-  }
-
+  isMatch ? disableCards() : unflipCards();
 }
 
 // Handle match
 function disableCards() {
 
-  // Increment score  
-  if (currentPlayer === player1) {
-    player1.score++;
-  } else {
-    player2.score++;
-    switchPlayer();
-  }
+  // Increment score
+  currentPlayer.score++;
+  console.log(currentPlayer.name, 'score increased!');
 
   // Update DOM
   player1ScoreElement.textContent = player1.score;
@@ -112,36 +68,26 @@ function disableCards() {
   
   // Reset board
   resetBoard();
-
 }
 
-// Unflip cards
+// Handle no match
 function unflipCards() {
-
-  // Lock board
   lockBoard = true;
 
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
-    
-    resetBoard();
 
+    resetBoard();
   }, 1500);
-  switchPlayer();
 }
 
-// Reset board
+// Reset board 
 function resetBoard() {
-  hasFlippedCard = false;
-  lockBoard = false;
-  firstCard = null;
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+  console.log('Board reset!'); 
 }
 
 // Attach event listener
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-// Restart game
-function restartGame() {
-  // Reset logic
-}
